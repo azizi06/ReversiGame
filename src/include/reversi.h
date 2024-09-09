f9ea545a2960f83a9586635361a66953c6704d77
AZIZImohammed <artazizi06@gmail.com>
2024-09-09 18:24:00 +0100

    Fixing  new_reversi

--- src/include/reversi.h
+++ src/include/reversi.h
@@ -10,14 +10,16 @@ struct Reversi
     int game_matrix[ROWS][COLUMNS] ;
     char player ;
     bool winner;
-    void (*move)  (struct Move* move);
+    void (*move)  (struct Reversi *self,struct Move* move);
+    void (*next) (struct Reversi *self);
+    void (*check_winner) (struct Reversi *self);
     
 };
 struct Reversi* new_reversi(char player);
 struct set* avaible_actions(int game_mateix[ROWS][COLUMNS]);
-void perform_move(struct Move* move);
-bool check_winner(int game_matrix[ROWS][COLUMNS]);
-char next_player(int gamme_matrix[ROWS][COLUMNS]);
+void perform_move(struct Reversi *self,struct Move* move);
+bool check_winner(struct Reversi *self);
+char next_player(struct Reversi *self);
 
 
 #endif
--- src/src/reversi.c
+++ src/src/reversi.c
@@ -4,17 +4,21 @@
 #include <stdio.h>
 struct Reversi* new_reversi(char player);
 struct set* avaible_actions(int game_mateix[ROWS][COLUMNS]);
-void perform_move(struct Move* move);
-bool check_winner(int game_matrix[ROWS][COLUMNS]);
-char next_player(int gamme_matrix[ROWS][COLUMNS]);
+void perform_move(struct Reversi *self,struct Move* move);
+bool check_winner(struct Reversi *self);
+char next_player(struct Reversi *self);
 
 struct Reversi* new_reversi(char player){
     struct Reversi* r = (struct Reversi*) malloc(sizeof(struct Reversi));
     r->player = player;
-    r->move = &perform_move;
     r->winner = false; 
+    r->move = &perform_move;
+    r->next = &next_player;
+    r->check_winner = &check_winner;
 }
 struct set* avaible_actions(int game_mateix[ROWS][COLUMNS]);
-void perform_move(struct Move* move);
-bool check_winner(int game_matrix[ROWS][COLUMNS]);
-char next_player(int gamme_matrix[ROWS][COLUMNS]);
+void perform_move(struct Reversi *self,struct Move* move){
+
+}
+bool check_winner(struct Reversi *self);
+char next_player(struct Reversi *self);
